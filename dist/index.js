2d427d09604028a5034dcfffa6ab825d21e2a1b0
ALazyTiger <35130215+ALazyTiger@users.noreply.github.com>
2020-11-26 16:56:31 +0800

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,42 +1,39 @@
-
-  'use strict'
-
-  var fullpage = {}
-  var opt = {
-    start: 0,
-    duration: 500,
-    loop: false,
-    dir: 'v',
-    der: 0.1,
-    current:0,
-    movingFlag: false,
-    preventWechat: false,
-    beforeChange: function(data) {},
-    afterChange: function(data) {}
-  }
-  fullpage.install = function (Vue, options) {
-    var that = fullpage
-    Vue.directive('fullpage', {
-      inserted: function (el, binding, vnode) {
-        var opts = binding.value || {}
-        that.init(el, opts, vnode)
-      },
-      componentUpdated: function (el, binding, vnode) {
-        var opts = binding.value || {}
-        that.init(el, opts, vnode)
+'use strict'
+var fullscreen = {}
+var opt = {
+  start: 0,
+  loop: false,
+  direction: 'v',
+  duration: 500,
+  distance: 0.1,
+  movingFlag: false,
+  preventWechat: false,
+  beforeChange: function (data) { },
+  afterChange: function (data) { }
+}
+fullscreen.install = function (Vue, options) {
+  var that = fullscreen
+  Vue.directive('fullscreen', {
+    inserted: function (el, binding, vnode) {
+      var opts = binding.value || {}
+      that.init(el, opts, vnode)
+    },
+    componentUpdated: function (el, binding, vnode) {
+      var opts = binding.value || {}
+      that.init(el, opts, vnode)
+    }
+  })
+  Vue.directive('animate', {
+    inserted: function (el, binding, vnode) {
+      if (binding.value) {
+        that.initAnimate(el, binding, vnode)
       }
-    })
-    Vue.directive('animate', {
-      inserted: function (el, binding, vnode) {
-        if (binding.value) {
-          that.initAnimate(el, binding, vnode)
-        }
-      }
-    })
-  }
+    }
+  })
+}
 
-fullpage.initAnimate = function (el, binding, vnode) {
-  var that = fullpage;
+fullscreen.initAnimate = function (el, binding, vnode) {
+  var that = fullscreen;
   var vm = vnode.context;
   var aminate = binding.value;
   el.style.opacity = '0';
@@ -46,6 +43,7 @@ fullpage.initAnimate = function (el, binding, vnode) {
       parent = parent.parentNode;
     }
     var curPage = +parent.getAttribute('data-id');
+
     if (curIndex === curPage) {
       that.addAnimated(el, aminate);
     } else {
@@ -54,187 +52,194 @@ fullpage.initAnimate = function (el, binding, vnode) {
     }
   });
 };
-  fullpage.addAnimated = function (el, animate) {
-    var delay = animate.delay || 0
-    // console.log(el)
-    el.classList.add('animated')
+fullscreen.addAnimated = function (el, animate) {
+  var delay = animate.delay || 0
+  // console.log(el)
+  el.classList.add('animated')
 
-    window.setTimeout(function () {
-      el.style.opacity = '1'
-      console.log("that", fullpage.curIndex)
-      el.classList.add(animate.value)
-    }, delay)
+  window.setTimeout(function () {
+    el.style.opacity = '1'
+    el.classList.add(animate.value)
+  }, delay)
+}
+
+fullscreen.removeAnimated = function (el, animate) {
+  if(el.getAttribute('class').indexOf('animated') > -1) {
+    el.classList.remove(animate.value)
   }
+}
 
-  fullpage.removeAnimated = function (el, animate) {
-    if (el.getAttribute('class').indexOf('animated') > -1) {
-      el.classList.remove(animate.value)
+fullscreen.assignOpts = function (option) {
+  var that = fullscreen
+  var o = option || {}
+  for (var key in opt) {
+    if (!o.hasOwnProperty(key)) {
+      o[key] = opt[key]
     }
   }
+  that.o = o
+}
 
-  fullpage.assignOpts = function (option) {
-    var that = fullpage
-    var o = option || {}
-    for (var key in opt) {
-      if (!o.hasOwnProperty(key)) {
-        o[key] = opt[key]
+fullscreen.initScrollDirection = function () {
+  if (this.o.direction !== 'v') {
+    this.el.classList.add('fullscreen-wp-h')
+  }
+}
+
+fullscreen.init = function (el, options, vnode) {
+  var that = fullscreen
+  that.assignOpts(options)
+
+  that.vm = vnode.context
+  that.vm.$fullscreen = that
+  if (typeof (that.curIndex) == "undefined") {
+    that.curIndex = that.o.start;
+
+  }
+  that.startY = 0
+  that.deltaX = 0
+  that.deltaY = 0
+  that.o.movingFlag = false
+
+  that.el = el
+  that.el.classList.add('fullscreen-wp')
+
+  that.parentEle = that.el.parentNode
+  that.parentEle.classList.add('fullscreen-container')
+
+  that.pageEles = that.el.children
+  that.total = that.pageEles.length
+
+  that.initScrollDirection()
+  window.setTimeout(function () {
+    that.width = that.parentEle.offsetWidth
+    that.height = that.parentEle.offsetHeight
+    for (var i = 0; i < that.pageEles.length; i++) {
+      var pageEle = that.pageEles[i]
+      pageEle.setAttribute('data-id', i)
+      pageEle.classList.add('page')
+      pageEle.style.width = that.width + 'px'
+      pageEle.style.height = that.height + 'px'
+      that.initEvent(pageEle)
+    }
+    // that.el.classList.add('anim');
+
+    that.moveTo(that.curIndex, false)
+
+  }, 0)
+}
+
+fullscreen.initEvent = function (el) {
+  var that = fullscreen
+  that.prevIndex = that.curIndex
+  el.addEventListener('touchstart', function (e) {
+    if (that.o.movingFlag) {
+      return false
+    }
+    that.startX = e.targetTouches[0].pageX
+    that.startY = e.targetTouches[0].pageY
+  })
+  el.addEventListener('mousewheel', function (e) {
+    if (that.o.movingFlag) {
+      return false
+    }
+    that.deltaX = e.deltaX
+    that.deltaY = e.deltaY
+    var direction = that.o.direction
+    if (that.deltaY > 0) {
+      var nextIndex = that.curIndex + 1
+
+      if (nextIndex < that.total) {
+        that.moveTo(nextIndex, true)
+      }
+    } else {
+      var nextIndex = that.curIndex - 1
+      if (nextIndex >= 0) {
+        that.moveTo(nextIndex, true)
       }
     }
-    that.o = o
-  }
-
-  fullpage.initScrollDirection = function () {
-    if (this.o.dir !== 'v') {
-      this.el.classList.add('fullpage-wp-h')
+  })
+  el.addEventListener('touchend', function (e) {
+    if (that.o.movingFlag) {
+      return false
     }
-  }
+    var direction = that.o.direction
+    var sub = direction === 'v' ? (e.changedTouches[0].pageY - that.startY) / that.height : (e.changedTouches[0].pageX - that.startX) / that.width
+    var distance = sub > that.o.distance ? -1 : sub < -that.o.distance ? 1 : 0
+    // that.curIndex推迟到moveTo执行完之后再更新
+    var nextIndex = that.curIndex + distance
 
-  fullpage.init = function (el, options, vnode) {
-    var that = fullpage
-    that.assignOpts(options)
-
-    that.vm = vnode.context
-    that.vm.$fullpage = that
-    if (typeof (that.curIndex) == "undefined") {
-      that.curIndex = that.o.start
-    }
-    that.startY = 0
-    that.deltaX = 0
-    that.deltaY = 0
-    that.o.movingFlag = false
-
-    that.el = el
-    that.el.classList.add('fullpage-wp')
-
-    that.parentEle = that.el.parentNode
-    that.parentEle.classList.add('fullpage-container')
-
-    that.pageEles = that.el.children
-    that.total = that.pageEles.length
-
-    that.initScrollDirection()
-    window.setTimeout(function () {
-      that.width = that.parentEle.offsetWidth
-      that.height = that.parentEle.offsetHeight
-
-      for (var i = 0; i < that.pageEles.length; i++) {
-        var pageEle = that.pageEles[i]
-        pageEle.setAttribute('data-id', i)
-        pageEle.classList.add('page')
-        pageEle.style.width = that.width + 'px'
-        pageEle.style.height = that.height + 'px'
-        that.initEvent(pageEle)
-      }
-      that.moveTo(that.curIndex, false)
-    }, 0)
-  }
-
-  fullpage.initEvent = function (el) {
-    var that = fullpage
-    that.prevIndex = that.curIndex
-    el.addEventListener('touchstart', function (e) {
-      console.log("touchstart",e)
-      if (that.o.movingFlag) {
-        return false
-      }
-      that.startX = e.targetTouches[0].pageX
-      that.startY = e.targetTouches[0].pageY
-    })
-    el.addEventListener('mousewheel', function (e) {
-      if (that.o.movingFlag) {
-        return false
-      }
-      that.deltaX = e.deltaX
-      that.deltaY = e.deltaY
-      var dir = that.o.dir
-      if( that.deltaY > 0){
-        var nextIndex = that.curIndex + 1
-        
-        if (nextIndex < that.total) {
-          that.moveTo(nextIndex, true)
-        }
-      }else{
-        var nextIndex = that.curIndex - 1
-        if (nextIndex >= 0) {
-          that.moveTo(nextIndex, true)
-        }
-      }
-    })
-    el.addEventListener('touchend', function(e) {
-      if (that.o.movingFlag) {
-        return false
-      }
-      var dir = that.o.dir
-      var sub = dir === 'v' ? (e.changedTouches[0].pageY - that.startY) / that.height : (e.changedTouches[0].pageX - that.startX) / that.width
-      var der = sub > that.o.der ? -1 : sub < -that.o.der ? 1 : 0
-      // that.curIndex推迟到moveTo执行完之后再更新
-      var nextIndex = that.curIndex + der
-
-      if (nextIndex >= 0 && nextIndex < that.total) {
+    if (nextIndex >= 0 && nextIndex < that.total) {
+      that.moveTo(nextIndex, true)
+    } else {
+      if (that.o.loop) {
+        nextIndex = nextIndex < 0 ? that.total - 1 : 0
         that.moveTo(nextIndex, true)
       } else {
-        if (that.o.loop) {
-          nextIndex = nextIndex < 0 ? that.total - 1 : 0
-          that.moveTo(nextIndex, true)
-        } else {
-          that.curIndex = nextIndex < 0 ? 0 : that.total - 1
-        }
+        that.curIndex = nextIndex < 0 ? 0 : that.total - 1
       }
-    })
-    if (that.o.preventWechat) {
-      el.addEventListener('touchmove', function(e) {
-        e.preventDefault()
-      })
     }
+  })
+  if (that.o.preventWechat) {
+    el.addEventListener('touchmove', function (e) {
+      e.preventDefault()
+    })
   }
+}
 
-fullpage.moveTo = function (curIndex, anim) {
-  var that = fullpage;
-  var dist =
-    that.o.dir === 'v' ? curIndex * -that.height : curIndex * -that.width;
+fullscreen.moveTo = function (curIndex, anim) {
+  var that = fullscreen;
+  var dist = that.o.direction === 'v' ? curIndex * - that.height : curIndex * - that.width;
   that.o.movingFlag = true;
-  var flag = that.o.beforeChange(that.prevIndex, curIndex);
-  if (flag === false) {
-    // 重置movingFlag
-    that.o.movingFlag = false;
-    return false;
-  }
-  that.curIndex = curIndex;
-
   if (anim) {
-    that.el.classList.add('anim');
+    var flag = that.o.beforeChange(that.prevIndex, curIndex);
+    if (flag === false) {
+      // 重置movingFlag
+      that.o.movingFlag = false;
+      return false;
+    }
+    that.curIndex = curIndex;
+    if (anim) {
+      that.el.classList.add('anim');
+    } else {
+      that.el.classList.remove('anim');
+    }
+    that.move(dist);
+    var preValue = that.prevIndex;
+    window.setTimeout(function () {
+      that.o.afterChange(preValue, curIndex);
+      that.o.movingFlag = false;
+      that.prevIndex = curIndex;
+      that.vm.$emit('toogle_animate', curIndex);
+    }, that.o.duration);
   } else {
-    that.el.classList.remove('anim');
+    window.setTimeout(function () {
+      that.o.movingFlag = false;
+      that.vm.$emit('toogle_animate', curIndex);
+    }, that.o.duration);
   }
-
-  that.move(dist);
-  window.setTimeout(function () {
-    that.o.afterChange(that.prevIndex, curIndex);
-    that.o.movingFlag = false;
-    that.prevIndex = curIndex;
-    that.vm.$emit('toogle_animate', curIndex);
-  }, that.o.duration);
 };
 
-  fullpage.move = function (dist) {
-    var xPx = '0px'
-    var yPx = '0px'
-    if (this.o.dir === 'v') {
-      yPx = dist + 'px'
-    } else {
-      xPx = dist + 'px'
-    }
-    this.el.style.cssText += ('-webkit-transform:translate3d(' + xPx + ', ' + yPx + ', 0px);transform:translate3d(' + xPx + ', ' + yPx + ', 0px);')
-  }
+fullscreen.move = function (dist) {
 
-  if (typeof exports === 'object') {
-    module.exports = fullpage
-  } else if (typeof define === 'function' && define.amd) {
-    define([], function() {
-      return fullpage
-    })
-  } else if (window.Vue) {
-    window.VueFullpage = fullpage
-    Vue.use(fullpage)
+  var xPx = '0px'
+  var yPx = '0px'
+  if (this.o.direction === 'v') {
+    yPx = dist + 'px'
+  } else {
+    xPx = dist + 'px'
   }
-export default fullpage;
+  this.el.style.cssText += ('-webkit-transform:translate3d(' + xPx + ', ' + yPx + ', 0px);transform:translate3d(' + xPx + ', ' + yPx + ', 0px);')
+}
+
+if (typeof exports === 'object') {
+  module.exports = fullscreen
+} else if (typeof define === 'function' && define.amd) {
+  define([], function () {
+    return fullscreen
+  })
+} else if (window.Vue) {
+  window.Vuefullscreen = fullscreen
+  Vue.use(fullscreen)
+}
+export default fullscreen;
